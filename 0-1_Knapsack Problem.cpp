3ef392e202ba1cb71a3a96bb9840bf7b64ac9867
Bholanath Barik <71944044+bholanathbarik9748@users.noreply.github.com>
2021-10-27 13:40:53 +0530

    Update 0-1_Knapsack Problem.cpp

--- 0-1_Knapsack Problem.cpp
+++ 0-1_Knapsack Problem.cpp
@@ -5,25 +5,22 @@ using namespace std;
 
 int knapSack(int W, int wt[], int val[], int n)
 {
-    int dp[n + 1][W + 1];
-
-    for (int i = 0; i <= n; i++)
+    int i, j;
+    int K[n + 1][W + 1];
+    for (i = 0; i <= n; i++)
     {
-        for (int j = 0; j <= W; i++)
+        for (j = 0; j <= W; j++)
         {
-            if (i == 0 or j == 0)
-            {
-                dp[i][j] = 0;
-            }
+            if (i == 0 || j == 0)
+                K[i][j] = 0;
             else if (wt[i - 1] <= j)
-            {
-                dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
-            }
+                K[i][j] = max(val[i - 1] + K[i - 1][j - wt[i - 1]],
+                              K[i - 1][j]);
             else
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
+                K[i][j] = K[i - 1][j];
         }
     }
-    return dp[n][W];
+
+    return K[n][W];
 }
+
